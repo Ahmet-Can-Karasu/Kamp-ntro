f64ecf922dbd9a019502000ce26f25da9eae3006
Ahmet Can karasu <karasu_ahmetcan@hotmail.com>
2021-01-23 23:51:37 +0300

    23 ocak ders sonu

---
+++ OOP2/MusteriManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{
+    // İş sınıflarıdır
+    class MusteriManager
+    {
+
+        public void Ekle(Musteri musteri)
+        {
+            //if ()
+            //{
+
+            //}
+
+
+        
+        }
+    }
+}
---
+++ OOP3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager ihtiyacKrediManager = new İhtiyacKrediManager();
+            IKrediManager tasitKrediManager = new TasitKrediManager();
+            IKrediManager konutKrediManager = new KonutKrediManager();
+
+            ILoggerService databaseLoggerServise = new DatabaseLoggerService();
+            ILoggerService fileLoggerServise = new FilebaseLoggerService();
+
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(tasitKrediManager,fileLoggerServise);
+
+
+
+            List<IKrediManager> krediler = new List<IKrediManager>() 
+            {ihtiyacKrediManager,tasitKrediManager };
+
+            //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+
+        }
+    }
+}
